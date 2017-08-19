9f5fdc3d7f3eed77bb6426e52312be2c3c7921fb
Lynachen <lynachen@126.com>
2017-08-19 11:00:59 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -5,7 +5,13 @@ $(function(){
 		$(this).find('.two-menu').show();
 	},function(){
 		$(this).find('.one-menu').removeClass('active');
-        $(this).find('.one-menu').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+		$(this).find('.one-menu').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
 		$(this).find('.two-menu').hide();		
 	});
+
+	//英文版首页轮播图
+	$('#myCarousel').carousel({
+		interval: 3000
+	});
+
 });
