10978e9f9419d503face1ad8999c78b266000e11
Lynachen <lynachen@126.com>
2018-09-21 17:15:58 +0800

    update

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".menu li").hover(function(){
+		$(this).find('.one-menu').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+		$(this).find('.two-menu').show();
+	},function(){
+		$(this).find('.one-menu').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+		$(this).find('.two-menu').hide();		
+	});
+
+	//英文版首页轮播图
+	// $('#myCarousel').carousel({
+	// 	interval: 3000
+	// });
+
+});
