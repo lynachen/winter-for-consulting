72b7464ed70f0da844c6920d95bf2d30b4653cc0
chenlingna <chenlingna@jingke.com>
2021-01-04 09:45:14 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,6 @@
 $(function(){
+	$("#header-wrapper").load("header.html");
+	$("#footer-wrapper").load("footer.html");
 	$(".menu li").hover(function(){
 		$(this).find('.one-menu').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
 		$(this).find('.two-menu').show();
@@ -6,10 +8,4 @@ $(function(){
 		$(this).find('.one-menu').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
 		$(this).find('.two-menu').hide();		
 	});
-
-	//英文版首页轮播图
-	// $('#myCarousel').carousel({
-	// 	interval: 3000
-	// });
-
 });
